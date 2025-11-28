2d23dccb3f486e39741d8450432579fcadc13e92
BobSoft <jensbosman2012@gmail.com>
2025-11-28 13:25:16 +0100

    Update dog.lua

--- dog.lua
+++ dog.lua
@@ -9,14 +9,20 @@ end
 
 i = 1
 store = 0
+input = ""
 
 while i <= #lines do
 	if lines[i] == "dog" then
 		store = tonumber(lines[i+1]) + tonumber(lines[i-1])
 		if store == 1 then
-			io.write(string.char(lines[i+2]))
-		if store == 2 then
+			io.write(string.char(tonumber(lines[i+2]))
+		elseif store == 2 then
 			i = lines[i+2]
+		elseif store == 3 then
+			io.write("")
+			input = tonumber(io.read())
+			i = 0
+			lines[1] = input
 		else
 			io.write(store)
 		end
